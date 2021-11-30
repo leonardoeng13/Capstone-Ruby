c67f9d84d7da1446b9de2c1860c9d3715e90a034
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-11-30 22:47:46 +0500

    Preserve data in JSON format

--- library/music/music_album.rb
+++ library/music/music_album.rb
@@ -12,4 +12,12 @@ class MusicAlbum < Item
   def can_be_archived
     return true if @on_spotify && super
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'on_spotify' => @on_spotify
+      'publish_date' => @publish_date,
+    }.to_json(*args)
+  end
 end
