2e444abe08f2b6af5a1ec4ce46e5a1195ea52dcb
herokudev <ao43085@gmail.com>
2021-12-01 15:17:15 -0600

    Add movie test
    Add movie test

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,25 +1,25 @@
-# frozen_string_literal: true
-
 require_relative '../library/movie/movie'
 
-describe Movie do
-  before(:each) do
-    @movie = Movie.new(publish_date: '2020-10-10', silent: false, archived: false, name: 'Test')
-  end
-
-  it 'shows instance of Movie class' do
-    expect(@movie).to be_instance_of Movie
-  end
-
-  it 'show inheritance from person class' do
-    expect(@movie).to be_kind_of Item
-  end
-
-  it 'verifies archived return false' do
-    expect(@movie.archived).to be false
-  end
-
-  it 'verifies silent return true' do
-    expect(@movie.silent).to be false
+describe 'Test1 for movies' do
+  it 'verifies silent return false' do
+      myMovie = Movie.new(false,'2021-12-01','Hancok')
+      result = myMovie.silent
+      expect(result).to be false
   end
 end
+
+describe 'Test2 for movies' do
+  it 'verifies silent return false' do
+      myMovie = Movie.new(false,'2021-12-01','Hancok')
+      expect(myMovie).to be_instance_of Movie
+  end
+end
+
+describe 'Test2 for movies' do
+  it 'verifies silent return false' do
+      myMovie = Movie.new(false,'2021-12-01','Hancok')
+      expect(myMovie).to be_kind_of Item
+  end
+end
+
+
