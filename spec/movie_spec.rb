5e940293b54ca85960f8c97f33fe24065a749fe4
Laylo Khodjaeva <khodjaeva.laylo@gmail.com>
2021-12-02 03:37:09 +0500

    Fix linter error

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -2,22 +2,22 @@ require_relative '../library/movie/movie'
 
 describe 'Test1 for movies' do
   it 'verifies silent return false' do
-    myMovie = Movie.new(false, '2021-12-01', 'Hancok')
-    result = myMovie.silent
+    my_movie = Movie.new(false, '2021-12-01', 'Hancok')
+    result = my_movie.silent
     expect(result).to be false
   end
 end
 
 describe 'Test2 for movies' do
   it 'verifies silent return false' do
-    myMovie = Movie.new(false, '2021-12-01', 'Hancok')
-    expect(myMovie).to be_instance_of Movie
+    my_movie = Movie.new(false, '2021-12-01', 'Hancok')
+    expect(my_movie).to be_instance_of Movie
   end
 end
 
 describe 'Test2 for movies' do
   it 'verifies silent return false' do
-    myMovie = Movie.new(false, '2021-12-01', 'Hancok')
-    expect(myMovie).to be_kind_of Item
+    my_movie = Movie.new(false, '2021-12-01', 'Hancok')
+    expect(my_movie).to be_kind_of Item
   end
 end
