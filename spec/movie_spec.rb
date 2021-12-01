54b63ebf0b40a0daa22c13fc414187be7aa32ede
herokudev <ao43085@gmail.com>
2021-12-01 16:35:32 -0600

    Fixing linter errors
    Fixing linter errors

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -2,24 +2,22 @@ require_relative '../library/movie/movie'
 
 describe 'Test1 for movies' do
   it 'verifies silent return false' do
-      myMovie = Movie.new(false,'2021-12-01','Hancok')
-      result = myMovie.silent
-      expect(result).to be false
+    myMovie = Movie.new(false, '2021-12-01', 'Hancok')
+    result = myMovie.silent
+    expect(result).to be false
   end
 end
 
 describe 'Test2 for movies' do
   it 'verifies silent return false' do
-      myMovie = Movie.new(false,'2021-12-01','Hancok')
-      expect(myMovie).to be_instance_of Movie
+    myMovie = Movie.new(false, '2021-12-01', 'Hancok')
+    expect(myMovie).to be_instance_of Movie
   end
 end
 
 describe 'Test2 for movies' do
   it 'verifies silent return false' do
-      myMovie = Movie.new(false,'2021-12-01','Hancok')
-      expect(myMovie).to be_kind_of Item
+    myMovie = Movie.new(false, '2021-12-01', 'Hancok')
+    expect(myMovie).to be_kind_of Item
   end
 end
-
-
